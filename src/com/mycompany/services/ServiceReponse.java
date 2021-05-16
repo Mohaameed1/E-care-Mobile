831d9922d587947cc29a8ace22e38746898d6a62
Mohaameed1 <mohamed.amorri@esprit.tn>
2021-05-16 18:19:37 +0100

    amorri

---
+++ src/com/mycompany/services/ServiceReponse.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.mycompany.entities.Reponse;
+import com.mycompany.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author bhk
+ */
+public class ServiceReponse{
+
+    public ArrayList<Reponse> tasks;
+    
+    public static ServiceReponse instance=null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+
+    private ServiceReponse() {
+         req = new ConnectionRequest();
+    }
+
+    public static ServiceReponse getInstance() {
+        if (instance == null) {
+            instance = new ServiceReponse();
+        }
+        return instance;
+    }
+
+    public boolean addTask(Reponse t) {
+         String url = Statics.BASE_URL + "/message/addReponseJSON/new?pseudo=" + t.getPseudo() + "&rep="+ t.getRep()+ "&question=" + t.getQuestion(); //création de l'URL
+       //création de l'URL
+        req.setUrl(url);
+    req.setPost(false); // Insertion de l'URL de notre demande de connexion
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this); //Supprimer cet actionListener
+                /* une fois que nous avons terminé de l'utiliser.
+                La ConnectionRequest req est unique pour tous les appels de 
+                n'importe quelle méthode du Service task, donc si on ne supprime
+                pas l'ActionListener il sera enregistré et donc éxécuté même si 
+                la réponse reçue correspond à une autre URL(get par exemple)*/
+                
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+    }
+
+    public ArrayList<Reponse> parseTasks(String jsonText){
+        try {
+            tasks=new ArrayList<>();
+            JSONParser j = new JSONParser();// Instanciation d'un objet JSONParser permettant le parsing du résultat json
+
+            /*
+                On doit convertir notre réponse texte en CharArray à fin de
+            permettre au JSONParser de la lire et la manipuler d'ou vient 
+            l'utilité de new CharArrayReader(json.toCharArray())
+            
+            La méthode parse json retourne une MAP<String,Object> ou String est 
+            la clé principale de notre résultat.
+            Dans notre cas la clé principale n'est pas définie cela ne veux pas
+            dire qu'elle est manquante mais plutôt gardée à la valeur par defaut
+            qui est root.
+            En fait c'est la clé de l'objet qui englobe la totalité des objets 
+                    c'est la clé définissant le tableau de tâches.
+            */
+            Map<String,Object> tasksListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            
+              /* Ici on récupère l'objet contenant notre liste dans une liste 
+            d'objets json List<MAP<String,Object>> ou chaque Map est une tâche.               
+            
+            Le format Json impose que l'objet soit définit sous forme
+            de clé valeur avec la valeur elle même peut être un objet Json.
+            Pour cela on utilise la structure Map comme elle est la structure la
+            plus adéquate en Java pour stocker des couples Key/Value.
+            
+            Pour le cas d'un tableau (Json Array) contenant plusieurs objets
+            sa valeur est une liste d'objets Json, donc une liste de Map
+            */
+            List<Map<String,Object>> list = (List<Map<String,Object>>)tasksListJson.get("root");
+            
+            //Parcourir la liste des tâches Json
+            for(Map<String,Object> obj : list){
+                //Création des tâches et récupération de leurs données
+                Reponse t = new Reponse();
+               float id =Float.parseFloat(obj.get("id").toString());
+                t.setId((int)id);
+                String Pseudo = (obj.get("pseudo").toString());
+                t.setPseudo((String)Pseudo);
+                String Rep = (obj.get("rep").toString());
+                t.setRep((String)Rep);
+                t.setQuestion(obj.get("question").toString());
+
+           
+               
+                //Ajouter la tâche extraite de la réponse Json à la liste
+                tasks.add(t);
+            }
+            
+            
+        } catch (IOException ex) {
+            
+        }
+         /*
+            A ce niveau on a pu récupérer une liste des tâches à partir
+        de la base de données à travers un service web
+        
+        */
+        return tasks;
+    }
+    
+    public ArrayList<Reponse> getAllTasks(){
+         String url = Statics.BASE_URL+"/message/yes";
+        req.setUrl(url);
+        req.setPost(false); 
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                tasks = parseTasks(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return tasks;
+    }
+
+    public boolean deleteReponse(int id){
+   String url = Statics.BASE_URL + "/message/deleteReponseJSON/" + id ; 
+         req.setUrl(url);
+     req.addResponseListener(new ActionListener<NetworkEvent>() {
+       @Override
+       public void actionPerformed(NetworkEvent evt) {
+      req.removeResponseListener(this);
+       }
+   });
+       NetworkManager.getInstance().addToQueueAndWait(req);
+       return resultOK;
+        
+        
+        }
+    public boolean updateReponse(Reponse t) {
+      String url = Statics.BASE_URL + "/message/updateReponseJSON/"+t.getId()+"?&pseudo="+ t.getPseudo()+ "&rep=" + t.getRep()+ "&question="+t.getQuestion();
+        //création de l'URL
+        req.setUrl(url);
+    req.setPost(false); // Insertion de l'URL de notre demande de connexion
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this); //Supprimer cet actionListener
+                /* une fois que nous avons terminé de l'utiliser.
+                La ConnectionRequest req est unique pour tous les appels de 
+                n'importe quelle méthode du Service task, donc si on ne supprime
+                pas l'ActionListener il sera enregistré et donc éxécuté même si 
+                la réponse reçue correspond à une autre URL(get par exemple)*/
+                
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+    }
+}
